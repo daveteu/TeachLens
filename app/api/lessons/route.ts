957c74d4edad66344dcae874543e81675106706b
Dave Teu <daveteu@Daves-iMac.local>
2026-07-05 22:47:01 +0800

    Configure static export

--- app/api/lessons/route.ts
+++
@@ -1,6 +0,0 @@
-import { NextResponse } from "next/server";
-import { lessonCatalog } from "../../lessonCatalog";
-
-export function GET() {
-  return NextResponse.json({ lessons: lessonCatalog });
-}
